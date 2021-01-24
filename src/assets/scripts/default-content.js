fa114f5c5230a624524054e68091db4a1454e911
chengxinsong <chengxinsong@bytedance.com>
2021-01-24 15:17:13 +0800

    feat: upgrade content

--- src/assets/scripts/default-content.js
+++ src/assets/scripts/default-content.js
@@ -1,15 +1,322 @@
 const DEFAULT_CONTENT = `
-# 示例文章：Google 搜索的即时自动补全功能究竟是如何“工作”的？
-> Google 搜索**自动补全功能**的强大，相信不少朋友都能感受到，它帮助我们更快地“补全”我们所要输入的搜索关键字。那么，它怎么知道我们要输入什么内容？它又是如何工作的？在这篇文章里，我们一起来看看。
+# 示例文章：JS引擎到底是怎么执行？(一)
+> js是一种非常灵活的语言，理解js引擎的执行过程对于我们学习js是非常有必要的。看了很多这方便文章，大多数是讲的是事件循环（event loop）或者变量提升的等，并没有全面分析其中的过程。所以觉得把这个js执行的详细过程整理一下，帮助更好的理解js，接下来我们一起来看看。
 
-## 使用自动补全
-Google 搜索的自动补全功能可以在 Google 搜索应用的大多数位置使用，包括 [Google](https://www.google.com/) 主页、适用于 IOS 和 Android 的 Google 应用，我们只需要在 Google 搜索框上开始键入关键字，就可以看到联想词了。
+<center>
+关注「松宝写代码」，精选好文，每日一题
+</center>
 
-![](https://gitee.com/yanglbme/resource/raw/master/doocs-md/juejin.gif)
+> 作者：saucxs ｜ songEagle
 
-在上图示例中，我们可以看到，输入关键字 \`juej\`，Google 搜索会联想到“掘金”、“掘金小册”、“绝句”等等，好处就是，我们无须输入完整的关键字即可轻松完成针对这些 topics 的搜索。
+2020，实「鼠」不易
+
+2021，「牛」转乾坤
+
+风劲潮涌当扬帆，任重道远须奋蹄！
+
+## 一、前言
+
+> 2020.12.23 立的 flag，每日一题，题目类型不限制，涉及到JavaScript，Node，Vue，React，浏览器，http，算法与数据结构等领域。
+
+本文是:JS引擎到底是怎么执行？(一)
+
+我们把这个分为2篇文章讲。
+
+本篇讲：语法分析和预编译阶段。
+
+下一篇讲：执行阶段。
+
+![每日一题](https://raw.githubusercontent.com/saucxs/full_stack_knowledge_list/master/daily-question/daily_question.png)
+
+## 二、概述
+
+js是一种非常灵活的语言，理解js引擎的执行过程对于我们学习js是非常有必要的。看了很多这方便文章，大多数是讲的是事件循环（event loop）或者变量提升的等，并没有全面分析其中的过程。所以觉得把这个js执行的详细过程整理一下，帮助更好的理解js。
+
+### 1.1基础概念
+
+js是单线程语言。
+
+在浏览器中一个页面永远只有一个线程在执行js脚本代码
+
+js是单线程怨言，但是代码解析是非常迅速的，不会发生解析阻塞。
+
+js是异步执行的，通过实践循环（event loop）方式实现的
 
 
+暂时我们不考虑事件循环（event loop），我们先来看这样一段代码，来确定我们是否理解js引擎的执行过程
+
+\`\`\`
+console.log(person)
+console.log(personFun)
+var person = "saucxs";
+console.log(person)
+function personFun() {
+    console.log(person)
+    var person = "songEagle";
+    console.log(person)
+}
+personFun()
+console.log(person)
+\`\`\`
+
+可以自己直接使用浏览器看出输出结果
+
+首先我们来分析一下上面的代码，虽然很多开发人员基本上都能答出来，但是还是要啰嗦一下。
+
+全面分析js引擎的执行过程，分为三个阶段
+
+1、语法分析
+
+2、预编译阶段
+
+3、执行阶段
+
+说明：
++ 浏览器先按照js的顺序加载script标签分隔的代码块，js代码块加载完毕之后，
++ 立刻进入到上面的三个阶段，
++ 然后再按照顺序找下一个代码块，
++ 再继续执行三个阶段，
++ 无论是外部脚本文件（不异步加载）还是内部脚本代码块，都是一样的，并且都在同一个全局作用域中。
+
+
+
+## 三、语法分析
+
+js的代码块加载完毕之后，会首先进入到语法分析阶段，该阶段的主要作用：
+
+分析该js脚本代码块的语法是否正确，如果出现不正确会向外抛出一个语法错误（syntaxError），停止改js代码的执行，然后继续查找并加载下一个代码块；如果语法正确，则进入到预编译阶段。
+
+类似的语法报错的如下图所示：
+
+![error错误](https://raw.githubusercontent.com/saucxs/full_stack_knowledge_list/master/daily-question/JavaScript/js-compile/error.png)
+
+
+## 四、预编译阶段
+
+js代码块通过语法分析阶段之后，语法都正确的下回进入预编译阶段。
+
+在分析预编译阶段之前，我们先来了解一下js的运行环境，运行环境主要由三种：
+
+1、全局环境（js代码加载完毕后，进入到预编译也就是进入到全局环境）
+
+2、函数环境（函数调用的时候，进入到该函数环境，不同的函数，函数环境不同）
+
+3、eval环境（不建议使用，存在安全、性能问题）
+
+
+每进入到一个不同的运行环境都会创建 一个相应的执行上下文（execution context），那么在一段js程序中一般都会创建多个执行上下文，js引擎会以栈的数据结构对这些执行进行处理，形成函数调用栈（call stack），栈底永远是全局执行上下文（global execution context），栈顶则永远时当前的执行上下文。
+
+
+
+### 4.1函数调用栈
+
+什么是函数调用栈？
+
+函数调用栈就是使用栈存取的方式进行管理运行环境，特点是先进后出，后进后出
+
+我们来分析一下简答的js代码来理解函数调用栈：
+
+\`\`\`
+function bar() {
+    var B_context = "bar saucxs";
+    function foo() {
+        var f_context = "foo saucxs";
+    }
+    foo()
+}
+bar()
+\`\`\`
+
+上面代码块通过语法分析后，进入预编译阶段，如图所示:
+
+![stack栈调用](https://raw.githubusercontent.com/saucxs/full_stack_knowledge_list/master/daily-question/JavaScript/js-compile/stack.png)
+
+1、首先进入到全局环境，创建全局执行上下文（global Execution Context ），推入到stack中；
+
+2、调用bar函数，进入bar函数运行环境，创建bar函数执行上下文（bar Execution Context），推入stack栈中；
+
+3、在bar函数内部调用foo函数，则再进入到foo函数运行环境中，创建foo函数执行上下文（foo Execution Context），如上图，由于foo函数内部没有再调用其他函数，那么则开始出栈；
+
+5、foo函数执行完毕之后，栈顶foo函数执行上下文（foo Execution Context）首先出栈；
+
+6、bar函数执行完毕，bar函数执行上下文（bar Execution Context）出栈；
+
+7、全局上下文（global Execution Cntext）在浏览器或者该标签关闭的时候出栈。
+
+说明：不同的运行环境执行都会进入到代码预编译和执行两个阶段，语法分析则在代码块加载完毕时统一检查语法。
+
+
+
+### 4.2创建执行上下文
+
+执行上下文可以理解成当前的执行环境，与该运行环境相对应。创建执行上下文的过程中，主要是做了下面三件事，如图所示：
+
+![执行上下文](https://raw.githubusercontent.com/saucxs/full_stack_knowledge_list/master/daily-question/JavaScript/js-compile/ec.jpg)
+
+1、创建变量对象（variable object）
+
+2、创建作用域链（scope chain）
+
+3、确定this的指向
+
+
+
+#### 4.2.1创建变量对象
+
+创建变量对象主要是经过以下过程，如图所示：
+
+![创建变量对象](https://raw.githubusercontent.com/saucxs/full_stack_knowledge_list/master/daily-question/JavaScript/js-compile/VO.jpg)
+
+1、创建arguments对象，检查当前上下文的参数，建立该对象的属性与属性值，仅在函数环境（非箭头函数）中进行的，全局环境没有此过程。
+
+2、检查当前上下文的函数声明，按照代码顺序查找，将找到的函数提前声明，如果当前上下文的变量对象没有该函数名属性，则在该变量对象以函数名建立一个属性，属性值则指向该函数所在堆内存地址引用，如果存在，则会被新的引用覆盖掉。
+
+3、检查当前上下文的变量声明，爱去哪找代码顺序查找，将找到的变量提前声明，如果当前上下文的变量对象没有变量名属性，则在该变量对象以变量名建立一个属性，属性值为undefined；如果存在，则忽略该变量声明。
+
+说明：在全局环境中，window对象就是全局执行上下文的变量对象，所有的变量和函数都是window对象的属性方法。
+
+所以函数声明提前和变量声明提升是在创建变量对象中进行的，且函数声明优先级高于变量声明。
+
+下面我们再来分析这个简单代码
+
+
+\`\`\`
+function fun(m,n){
+    var saucxs = 1;
+    function execution(){
+        console.log(saucxs)
+    }
+}
+fun(2,3)
+\`\`\`
+
+这里我们在全局环境中调用fun函数，创建fun的执行上下文，这里暂时不说作用域链以及this指向的问题。
+
+\`\`\`
+funEC = {
+    //变量对象
+    VO: {
+        //arguments对象
+        arguments: {
+            m: undefined,
+            n: undefined,
+            length: 2
+        },
+        //execution函数
+        execution: <execution reference>, 
+        //num变量
+        saucxs: undefined
+    },
+    //作用域链
+    scopeChain:[],
+    //this指向
+    this: window
+}
+\`\`\`
+
+1、funEC表示fun函数的执行上下文（fun Execution Context 简写为funEC）；
+
+2、funEC的变量对象中arguments属性，上面这样写只是为了理解，在浏览器中展示以类数组的方式展示的
+
+3、<execution reference>表示的是execution函数在堆内存地址的引用
+
+说明：创建变量对象发生在预编译阶段，还没有进入到执行阶段，该变量对象都不能访问的，因为此时的变量对象中的变量属性尚未赋值，值仍为undefined，只有在进行执行阶段，变量中的变量属性才进行赋值后，变量对象（Variable Object）转为活动对象（Active Object）后，才能进行访问，这个过程就是VO->AO过程。
+
+
+
+#### 4.2.2创建作用域链
+
+作用域链由当前执行环境的变量对象（未进入到执行阶段前）与上层环境的一系列活动对象组成，保证了当前执行还款对符合访问权限的变量和函数有序访问。
+
+理解清楚作用域链可以帮助我们理解js很多问题包括闭包问题等，下面我们结合一个例子来理解一下作用域链。
+
+\`\`\`
+var num = 30;
+function test() {
+    var a = 10;
+    function innerTest() {
+        var b = 20;
+        return a + b
+    }
+    innerTest()
+}
+test()
+\`\`\`
+
+在上面例子中，当执行到调用innerTest函数，进入到innerTest函数环境。全局执行上下文和test函数执行上下文已进入到执行阶段，innerTest函数执行上下文在预编译阶段创建变量对象，所以他们的活动对象和变量对象分别是AO（global），AO（test）和VO（innerTest），而innerTest的作用域链由当前执行环境的变量对象（未进入到执行阶段前）与上层环境的一系列活动对象组成，如下：
+
+\`\`\`
+innerTestEC = {
+    //变量对象
+    VO: {b: undefined}, 
+    //作用域链
+    scopeChain: [VO(innerTest), AO(test), AO(global)],  
+    //this指向
+    this: window
+}
+\`\`\`
+
+我们这里可以直接使用数组表示作用域链，作用域链的活动对象或者变量对象可以直接理解成作用域。
+
+1、作用域链的第一项永远是当前作用域（当前上下文的变量对象或者活动对象）；
+
+2、最后一项永远是全局作用域（全局上下文的活动对象）；
+
+3、作用域链保证了变量和函数的有序访问，查找方式是沿着作用域链从左至右查找变量或者函数，找到则会停止找，找不到则一直查找全局作用域，再找不到就会排除错误。
+
+
+
+#### 4.2.3闭包
+
+什么是闭包？思考一下
+
+看一下简单的例子
+
+\`\`\`
+function foo() {
+    var num = 20;
+    function bar() {
+        var result = num + 20;
+        return result
+    }
+    bar()
+}
+foo()
+\`\`\`
+
+因为对于闭包的有很多的不同理解，包括我看一些书籍（js高级程序设计），我这直接以浏览器解析，以浏览器的闭包为准来分析闭包，如图:
+
+![](https://raw.githubusercontent.com/saucxs/full_stack_knowledge_list/master/daily-question/JavaScript/js-compile/demo.png)
+
+如图所示，谷歌浏览器理解的闭包是foo，那么按照浏览器的标准是如何定义的闭包，自己总结为三点：
+
+1、在函数内部定义新函数
+
+2、新函数访问外层函数的局部变量，即访问外层函数环境的活动对象属性
+
+3、新函数执行，创建新函数的执行上下文，外层函数即为闭包
+
+
+
+#### 4.2.4确定this指向
+
+1、在全局环境下，全局执行的上下文中变量对象的this属性指向为window；
+
+2、在函数环境下的this指向比较灵活，需要根据执行环境和执行方法确定，列举典型例子来分析
+
+
+
+## 五、总结
+
+由于涉及到的内容过多，下一次将第三阶段（执行阶段）单独分离出来。另开出新文章详细分析，主要介绍js执行阶段中的同步任务执行和异步任务执行机制（事件循环（Event Loop））。
+
+
+## 六、参考
+
+你不知道的javascript(上卷)
+
+
+## 七、测试脚本
 这是一个脚注的测试 [^note]
 
 \`\`\`javascript
@@ -34,91 +341,108 @@ Google 搜索的自动补全功能可以在 Google 搜索应用的大多数位�
 \`\`\`
 
 
-谷歌搜索的自动补全功能对于使用移动设备的用户来说特别有用，用户可以轻松在难以键入的小屏幕上完成搜索。当然，对于移动设备用户和台式机用户而言，这都节省了大量的时间。根据 Google 官方报告，自动补全功能可以减少大约 25% 的打字，累积起来，预计每天可以节省 200 多年的打字时间。是的，每天！
+## 谢谢支持
 
-> 注意，本文所提到的“**联想词**”与“**预测**”，是同一个意思。
+1、文章喜欢的话**可以「分享，点赞，在看」三连**哦。
 
-## 基于“预测”而非“建议”
-Google 官方将自动补全功能称之为“预测”，而不是“建议”，为什么呢？其实是有充分理由的。自动补全功能是为了**帮助用户完成他们打算进行的搜索**，而不是建议用户要执行什么搜索。
+2、作者昵称：saucxs，songEagle，松宝写代码。「松宝写代码」公众号作者，每日一题，实验室等。一个爱好折腾，致力于全栈，正在努力成长的字节跳动工程师，星辰大海，未来可期。**内推字节跳动各个部门各个岗位。**
 
-那么，Google 是如何确定这些“预测”的？其实，Google 会根据趋势搜索 [trends](https://trends.google.com/trends/?geo=US) 给到我们这些“预测”。简单来说，哪个热门、哪个搜索频率高，就更可能推给我们。当然，这也与我们当前所处的位置以及我们的搜索历史相关。
+3、长按下面图片，关注「松宝写代码」，是获取开发知识体系构建，精选文章，项目实战，实验室，**每日一道面试题**，进阶学习，思考职业发展，涉及到JavaScript，Node，Vue，React，浏览器，http，算法，端相关，小程序等领域，希望可以帮助到你，我们一起成长～
 
-另外，这些“预测”也会随着我们键入的关键字的变更而更改。例如，当我们把键入的关键字从 \`juej\` 更改为 \`juex\` 时，与“掘金”相关的预测会“消失”，同时，与“觉醒”、“决心”相关联的词会出现。
+![松宝写代码](https://raw.githubusercontent.com/saucxs/full_stack_knowledge_list/master/daily-question/dongtai.gif?raw=true)
 
-![](https://gitee.com/yanglbme/resource/raw/master/doocs-md/juex.gif)
 
-## 为什么看不到某些联想词？
-如果我们在输入某个关键字时看不到联想词，那么表明 Google 的算法可能检测到：
+## 字节内推福利
++ 回复「校招」获取内推码
++ 回复「社招」获取内推
++ 回复「实习生」获取内推
 
-- 这个关键字不是热门字词；
-- 搜索的字词太新了，我们可能需要等待几天或几周才能看到联想词；
-- 这是一个侮辱性或敏感字词，这个搜索字词违反了 Google 的相关政策。更加详细的情况，可以了解 [Google 搜索自动补全政策](https://support.google.com/websearch/answer/7368877)。
+后续会有更多福利
 
-## 为什么会看到某些不当的联想词？
-Google 拥有专门设计的系统，可以自动捕获不适当的预测结果而不显示出来。然而，Google 每天需要处理数十亿次搜索，这意味着 Google 每天会显示数十亿甚至上百亿条预测。再好的系统，也可能存在缺陷，不正确的预测也可能随时会出现。
+## 学习资料福利
+回复「算法」获取算法学习资料
 
-我们作为 Google 搜索的用户，如果认定某条预测违反了相关的搜索自动补全政策，可以进行举报反馈，点击右下角“**举报不当的联想查询**”并勾选相关选项即可。
+## 推荐阅读
 
-![](https://gitee.com/yanglbme/resource/raw/master/doocs-md/report.gif)
+#### 1、JavaScript && ES6
 
-## 如何实现自动补全算法？
-目前，Google 官方似乎并没有公开搜索自动补全的算法实现，但是业界在这方面已经有了不少研究。
++ 第 28 题：[【每日一题】(28题)面试官:原型链与构造函数结合方法继承与原型式继承的区别？](https://mp.weixin.qq.com/s/uPUxo8gIGyHv-b_aWdgzaw)
 
-一个好的自动补全器必须是快速的，并且在用户键入下一个字符后立即更新联想词列表。**自动补全器的核心是一个函数，它接受输入的前缀，并搜索以给定前缀开头的词汇或语句列表**。通常来说，只需要返回少量的数目即可。
++ 第 22 题：[【每日一题】(22题)面试官问：var与const,let的主要区别是什么？](https://mp.weixin.qq.com/s/wJ1cG7eQw85fpqpk_fHq7w)
 
-接下来，我们先从一个简单且低效的实现开始，并在此基础上逐步构建更高效的方法。
++ 第 21 题：[【每日一题】(21题)面试官问：谈谈JS中的 this 的绑定？](https://mp.weixin.qq.com/s/WvDIjv_FNfDsD9OmB6SirA)
 
-### 词汇表实现
-一个**简单粗暴的实现方式**是：顺序查找词汇表，依次检查每个词汇，看它是否以给定的前缀开头。
++ 第 20 题：[【每日一题】(20题)面试官问：谈谈JS中的 webSockets 的理解？](https://mp.weixin.qq.com/s/GA-Wl03ZDLhnBCAG0wTi0w)
 
-但是，此方法需要将前缀与每个词汇进行匹配检查，若词汇量较少，这种方式可能勉强行得通。但是，如果词汇量规模较大，效率就太低了。
++ 第 19 题：[【每日一题】面试官问：谈谈JS中的 XMLHttpRequest 对象的理解？](https://mp.weixin.qq.com/s/wxIEGJVmfxq0Q-8E4tbv1A)
 
-一个**更好的实现方式是**：让词汇按字典顺序排序。借助二分搜索算法，可以快速搜索有序词汇表中的前缀。由于二分搜索的每一步都会将搜索的范围减半，因此，总的搜索时间与词汇表中单词数量的对数成正比，即时间复杂度是 \`O(log N)\`。二分搜索的性能很好，但有没有更好的实现呢？当然有，往下看。
++ 第 18 题：[【每日一题】面试官问：JS中的 Ajax 跨域与扩展 Comet ？](https://mp.weixin.qq.com/s/mb8TRlw1yzEOfDzMyYLW2g)
 
-### 前缀树实现
-通常来说，许多词汇都以相同的前缀开头，比如 \`need\`、\`nested\` 都以 \`ne\` 开头，\`seed\`、\`speed\` 都以 \`s\` 开头。要是为每个单词分别存储公共前缀似乎很浪费。
++ 第 17 题：[【每日一题】(17题)面试官问：JS中事件流，事件处理程序，事件对象的理解？](https://mp.weixin.qq.com/s/mb8TRlw1yzEOfDzMyYLW2g)
 
-![](https://gitee.com/yanglbme/resource/raw/master/doocs-md/pretree.png)
++ 第 16 题：[【每日一题】面试官问：JS中如何全面进行客户端检测？](https://mp.weixin.qq.com/s/-tNI1vwdK_SAxNGRQTCd1Q)
 
-前缀树是一种利用公共前缀来加速补全速度的数据结构。前缀树在节点树中排列一组单词，单词沿着从根节点到叶子节点的路径存储，树的层次对应于前缀的字母位置。
++ 第 15 题：[【每日一题】面试官问：JS类型判断有哪几种方法？](https://mp.weixin.qq.com/s/UwVgQMaVPg6Z0SVgn4kqwA)
 
-前缀的补全是顺着前缀定义的路径来查找的。例如，在上图的前缀树中，前缀 \`ne\` 对应于从子节点取左边缘 \`N\` 和唯一边缘 \`E\` 的路径。然后可以通过继续遍历从 \`E\` 节点可以达到的所有叶节点来生成补全列表。在图中，\`ne\` 的补全可以是两个分支：\`-ed\` 和 \`-sted\`。如果在数中找不到由前缀定义的路径，则说明词汇表中不包含以该前缀开头的单词。
++ 第 14 题：[【每日一题】面试官问：谈谈你对JS对象的创建和引申](https://mp.weixin.qq.com/s/-HTpVMFMRDu8sElNv-WqIw)
 
-### 有限状态自动机(DFA)实现
-前缀树可以有效处理公共前缀，但是，对于其他共享词部分，仍会分别存储在每个分支中。比如，后缀 \`ed\`、\`ing\`、\`tion\` 在英文单词中特别常见。在上一个例子中，\`e\`、\`d\` 分别存放在了每一个分支上。
++ 第 13 题[[每日一题]面试官问：['1', '2', '3'].map(parseInt)输出，原因，以及延伸？](https://mp.weixin.qq.com/s/DJ6Av4tQgJpqa8hKAPk_uA)
 
-有没有一种方法可以更加节省存储空间呢？有的，那就是 DFA。
++ 第 12 题[[每日一题]面试官问：JS引擎的执行过程（二）](https://mp.weixin.qq.com/s/CCUsCM2vzb6S1wcwIsjQuA)
 
-<center>
-<img src="https://gitee.com/yanglbme/resource/raw/master/doocs-md/dfa.gif" style="width: 50%;"></center>
++ 第 11 题[[每日一题]面试官问：JS引擎的执行过程（一）](https://mp.weixin.qq.com/s/Lhd5N5a1b8fAstWn5H3B3Q)
 
-在上面的例子中，单词 \`need\`、\`nested\`、\`seed\` 和 \`speed\` 仅由 9 个节点组成，而上一张图中的前缀树包含了 17 个节点。
++ 第 10 题[[每日一题]面试官问：详细说一下JS数据类型](https://mp.weixin.qq.com/s/wm0EGVXTTHoHMcdUxMQmKA)
 
-可以看出，最小化前缀树 DFA 可以在很大程度上减少数据结构的大小。即使词汇量很大，最小化 DFA 通常也适合在内存中存储，避免昂贵的磁盘访问是实现快速自动补全的关键。
++ 第 8 题[[每日一题]面试官问：谈谈你对ES6的proxy的理解？](https://mp.weixin.qq.com/s/8loJlarVrmj47XjgrZLI1w)
 
-### 一些扩展
-上面介绍了如何利用合理的数据结构实现基本的自动补全功能。这些数据结构可以通过多种方式进行扩展，从而改善用户体验。
++ 第 7 题[[每日一题]面试官问：for in和for of 的区别和原理？](https://mp.weixin.qq.com/s/RsynH85UkAwAgIAzwxs-Ag)
 
-通常，满足特定前缀的词汇可能很多，而用户界面上能够显示的却不多，我们更希望能显示最常搜索或者最有价值的词汇。这通常可以通过为词汇表中的每个单词增加一个代表单词值的**权重** \`weight\`，并且按照权重高低来排序自动补全列表。
++ 第 6 题[[每日一题]面试官问：Async/Await 如何通过同步的方式实现异步？](https://mp.weixin.qq.com/s/UAYBnQvekRugR8DVEUPB3Q)
 
-- 对于排序后的词汇表来说，在词汇表每个元素上增加 \`weight\` 属性并不难；
-- 对于前缀树来说，将 \`weight\` 存储在叶子节点中，也是很简单的一个实现；
-- 对于 \`DFA\` 来说，则较为复杂。因为一个叶子节点可以通过多条路径到达。一种解决方案是将权重关联到路径而不是叶子节点。
++ 第 3 道[「「每日一题」面试官问你对 Promise 的理解？可能是需要你能手动实现各个特性」](https://mp.weixin.qq.com/s/QuuPd2KCp50snN7F2o3oYg)
 
-目前有不少开源库都提供了这个功能，比如主流的搜索引擎框架 [Elasticsearch](https://www.elastic.co/products/elasticsearch)、[Solr](https://lucene.apache.org/solr/) 等，基于此，我们可以实现高效而强大的自动补全功能。
++ 第 2 道[「[每日一题]ES6 中为什么要使用 Symbol？」](https://mp.weixin.qq.com/s/omeVJdtabo5MeN3DItDfWg)
 
-#### 推荐阅读
-- [阿里又一个 20k+ stars 开源项目诞生，恭喜 fastjson！](https://mp.weixin.qq.com/s/RNKDCK2KoyeuMeEs6GUrow)
-- [刷掉 90% 候选人的互联网大厂海量数据面试题（附题解 + 方法总结）](https://mp.weixin.qq.com/s/rjGqxUvrEqJNlo09GrT1Dw)
-- [好用！期待已久的文本块功能究竟如何在 Java 13 中发挥作用？](https://mp.weixin.qq.com/s/kalGv5T8AZGxTnLHr2wDsA)
-- [2019 GitHub 开源贡献排行榜新鲜出炉！微软谷歌领头，阿里跻身前 12！](https://mp.weixin.qq.com/s/_q812aGD1b9QvZ2WFI0Qgw)
+#### 2、浏览器
+
++ 第 9 题[[每日一题]requestAnimationFrame不香吗？](https://mp.weixin.qq.com/s/4Ob_CEiZUyoHKxffAeAYdw)
+
+
+#### 3、Vue
+
++ 第 5 道[「每日一题」到底该如何回答：vue数据绑定的实现原理？](https://mp.weixin.qq.com/s/8eo4frdB-zMA7nD_1wdnLw)
+
+#### 4、HTML5
++ 第 29 道[【每日一题】(29题)面试官:HTML-HTML5新增标签属性的理解？](https://mp.weixin.qq.com/s/Lx5-bF-xliB9TBuEtE7dLw)
+
+#### 5、算法
++ 第 31 道[[【每日一题】(31题)面试官:你对图论了解多少？(一)](https://mp.weixin.qq.com/s/E6dh8A9dVxxB9jaRGm3kbg)
+
++ 第 27 道[【每日一题】(27题)算法题:如何使用多种解决方案来实现跳一跳游戏？](https://mp.weixin.qq.com/s/EY99dnyjjTvdBflpE5T2Fw)
+
++ 第 26 道[【每日一题】(26题)算法题:最长公共前缀？](https://mp.weixin.qq.com/s/1TzP0JgrzqXbQes1jzzwFg)
+
++ 第 25 道[【每日一题】(25题)算法题:堆数据结构-前 K 个高频元素？](https://mp.weixin.qq.com/s/desqLK9Wst9v7XPcNyvwlQ)
+
++ 第 24 道[【每日一题】(24题)算法题:贪心算法-环游世界之如何加油？](https://mp.weixin.qq.com/s/ST6pf00iBZiDs4GpGK0eOw)
+
++ 第 4 道[「每日一题」与面试官手撕代码：如何科学高效的寻找重复元素？](https://mp.weixin.qq.com/s/jFZ_2f272LhBBPuuLaWnyg)
+
+#### 6、Node
+
++ 第 23 道[【每日一题】(23题)面试官问：详细描述事件循环Event Loop？](https://mp.weixin.qq.com/s/hE-tK_PbSYkMms8P9b2H7A)
+
+#### 7、Http
+
++ 第 1 道[「一道面试题是如何引发深层次的灵魂拷问？」](https://mp.weixin.qq.com/s/O8j9gM5tD5rjLz1kdda3LA)
 
 ---
 
-欢迎关注我的公众号“**Doocs开源社区**”，原创技术文章第一时间推送。
+欢迎关注我的公众号“松宝写代码”，精选文章第一时间推送。
 
 <center>
-    <img src="https://gitee.com/yanglbme/resource/raw/master/doocs-md/qrcode.png" style="width: 100px;">
+    <img src="https://raw.githubusercontent.com/saucxs/full_stack_knowledge_list/master/image/songbao.png?raw=true" style="width: 100%;">
 </center>
 
 `;
